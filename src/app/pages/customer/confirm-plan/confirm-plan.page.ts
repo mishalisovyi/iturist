089ba8d0e19885d3dec44f9d25ac087e53d6ee3b
Yaroslav Yahimovich <apple@MyAppleComputer.local>
2019-03-01 18:04:43 +0200

    commit

--- src/app/pages/customer/confirm-plan/confirm-plan.page.ts
+++ src/app/pages/customer/confirm-plan/confirm-plan.page.ts
@@ -1,11 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
-// import { FormGroup, FormBuilder, Validators } from "@angular/forms";
 
 import { AlertController } from '@ionic/angular';
 import { InAppBrowser, InAppBrowserObject } from '@ionic-native/in-app-browser/ngx';
-
-// import * as moment from 'moment';
 
 import { Plan, BaseResponse } from "../../../models/models";
 
@@ -83,42 +80,20 @@ export class ConfirmPlanPage implements OnInit {
     }
   `;
 
-  // public creditCardForm: FormGroup;
-  // public payPalForm: FormGroup;
   public plan: Plan;
-  // public submitTry: boolean = false;
-  // public activeTabIndex: number = 0;
-  // public maxDatePickerValue: string = moment().add(10, "years").format();
 
   constructor(
     private router: Router,
     private route: ActivatedRoute,
-    // private formBuilder: FormBuilder,
     private alert: AlertController,
     private api: ApiService,
     private iab: InAppBrowser
   ) { }
 
   ngOnInit() {
-    // this.createForms();
     this.getPlanId();
     this.getPlan(this.planId);
   }
-
-  // private createForms() {
-  //   this.creditCardForm = this.formBuilder.group({
-  //     name: ["", Validators.required],
-  //     number: ["", [Validators.required, Validators.minLength(16), Validators.pattern('^[0-9]+$')]],
-  //     expire: ["", Validators.required],
-  //     cw: ["", [Validators.required, Validators.minLength(3), Validators.maxLength(3), Validators.pattern('^[0-9]+$')]]
-  //   });
-  //   this.payPalForm = this.formBuilder.group({
-  //     field1: ["", Validators.required],
-  //     field2: ["", Validators.required],
-  //     field3: ["", Validators.required],
-  //     field4: ["", Validators.required]
-  //   });
-  // }
 
   private getPlanId() {
     this.planId = this.route.snapshot.params.planId;
@@ -127,30 +102,6 @@ export class ConfirmPlanPage implements OnInit {
   private getPlan(id: string) {
     this.api.getPlan(id).subscribe((plan: BaseResponse) => this.plan = plan.content);
   }
-
-  // private async presentDialogAlert(reqData: any) {
-  //   const alert = await this.alert.create({
-  //     message: `
-  //       <p>Save this payment</p>
-  //       <p>for future payment in application?</p>
-  //     `,
-  //     buttons: [
-  //       {
-  //         text: 'Yes',
-  //         role: 'yes'
-  //       },
-  //       {
-  //         text: 'No',
-  //         role: 'no'
-  //       }
-  //     ]
-  //   });
-  //   alert.onDidDismiss().then((res: any) => {
-  //     reqData.save = res.role === 'yes' ? true : false;
-  //     this.api.confirmPlan(reqData).subscribe(() => this.presentInfoAlert());
-  //   });
-  //   await alert.present();
-  // }
 
   private async presentInfoAlert() {
     const alert = await this.alert.create({
@@ -164,10 +115,6 @@ export class ConfirmPlanPage implements OnInit {
     await alert.present();
   }
 
-  // public setActiveTab(index: number) {
-  //   this.activeTabIndex = index;
-  // }
-
   public confirmPlan() {
     this.browser = this.iab.create(
       "https://direct.tranzila.com/yoo/iframenew.php?hidesum=1&currency=1&tranmode=AK&sum=0.1",
@@ -178,20 +125,5 @@ export class ConfirmPlanPage implements OnInit {
     this.browser.on("loadstop").subscribe(() => {
       this.browser.insertCSS({ code: this.tranzilaCss }).then(() => this.browser.show());
     })
-    // this.browser.on("exit").subscribe((res: InAppBrowserEvent) => console.log("exit", res));
-    // this.browser.on("message").subscribe((res: InAppBrowserEvent) => console.log("message", res));
-
-    // this.submitTry = true;
-    // const form: FormGroup = this.activeTabIndex === 0 ? this.creditCardForm : this.payPalForm;
-    // if (form.valid) {
-    //   this.presentDialogAlert(form.value);
-    // }
   }
-
-  // public validateDate({ detail: { value: value } }: CustomEvent) {
-  //   this.creditCardForm.get("expire").setErrors(null);
-  //   if (moment(value) < moment()) {
-  //     this.creditCardForm.get("expire").setErrors({ expire: true });
-  //   }
-  // }
 }
